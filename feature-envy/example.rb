c5f38c5053dbedf483c957a0c6c139930fe44bec
Denys Medynskyi <denys.medynskyi@toptal.com>
2020-10-21 09:43:05 +0300

    Add feature envy: after

--- feature-envy/example.rb
+++ feature-envy/example.rb
@@ -4,3 +4,16 @@ class Warehouse
     (item.price - item.rebate) * @vat
   end
 end
+
+# after
+class Warehouse
+  def sale_price(item)
+    item.sale_price * @vat
+  end
+end
+
+class Item
+  def sale_price
+    price - rebate
+  end
+end
